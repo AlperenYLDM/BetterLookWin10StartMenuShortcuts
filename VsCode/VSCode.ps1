3c9e25f2ed0d3c11f326484f4ccc66a89a1d9874
Alperen YILDIRIM <alperen.yildirim36@protonmail.com>
2021-08-24 01:16:44 +0300

    Add files via upload

---
+++ VsCode/VSCode.ps1
@@ -0,0 +1,9 @@
+﻿$installloc = "C:\Users\$($env:USERNAME)\AppData\Local\Programs\Microsoft VS Code"
+$installxml = "$($installloc)\Code.VisualElementsManifest.xml"
+$install150logo = "$($installloc)\Assets\150x150Logo.png"
+$install70Logo = "$($installloc)\Assets\70x70Logo.png"
+wget 'https://raw.githubusercontent.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/main/VsCode/Code.VisualElementsManifest.xml' -OutFile $installxml
+New-Item -Path "$($installloc)" -Name "Assets" -ItemType "directory" | Out-Null
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/VsCode/Assets/150x150Logo.png' -OutFile $install150logo
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/VsCode/Assets/70x70Logo.png' -OutFile $install70Logo
+(ls "C:\Users\YıldırımPc\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk").lastwritetime = get-date
