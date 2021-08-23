77cd61a50fe6f135786a3fefc3ea3e58964c60be
Alperen YILDIRIM <alperen.yildirim36@protonmail.com>
2021-08-24 00:10:14 +0300

    Add files via upload

---
+++ Steam/Steam.ps1
@@ -0,0 +1,9 @@
+﻿$installloc = "C:\Program Files (x86)\Steam"
+$installxml = "$($installloc)\steam.VisualElementsManifest.xml" -replace " "*16
+$install150logo = "$($installloc)\Assets\150x150Logo.png" -replace " "*16
+$install70Logo = "$($installloc)\Assets\70x70Logo.png" -replace " "*16
+wget 'https://raw.githubusercontent.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/main/Steam/steam.VisualElementsManifest.xml' -OutFile $installxml
+New-Item -Path "$($installloc)" -Name "Assets" -ItemType "directory" | Out-Null
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/Steam/Assets/150x150Logo.png' -OutFile $install150logo
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/Steam/Assets/70x70Logo.png' -OutFile $install70Logo
+(ls "C:\Users\YıldırımPc\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Steam.lnk").lastwritetime = get-date
