206100f24009cc2eb896ab243b4f4c6a53a72de0
Alperen YILDIRIM <alperen.yildirim36@protonmail.com>
2021-08-24 00:03:07 +0300

    Add files via upload

---
+++ RockstarGamesLauncher/RockstarLauncher.ps1
@@ -0,0 +1,9 @@
+﻿$installloc = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object InstallLocation | Format-Table -AutoSize | findstr Rockstar
+$installxml = "$($installloc)\LauncherPatcher.VisualElementsManifest.xml" -replace " "*16
+$install150logo = "$($installloc)\Assets\150x150Logo.png" -replace " "*16
+$install70Logo = "$($installloc)\Assets\70x70Logo.png" -replace " "*16
+wget 'https://raw.githubusercontent.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/main/RockstarGamesLauncher/LauncherPatcher.VisualElementsManifest.xml' -OutFile $installxml
+New-Item -Path "$($installloc)" -Name "Assets" -ItemType "directory" | Out-Null
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/RockstarGamesLauncher/150x150Logo.png' -OutFile $install150logo
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/RockstarGamesLauncher/70x70Logo.png' -OutFile $install70Logo
+(ls "C:\Users\YıldırımPc\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Rockstar Games\Rockstar Games Launcher.lnk").lastwritetime = get-date
