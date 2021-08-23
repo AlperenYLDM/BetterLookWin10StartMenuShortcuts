daa1afd429f81f330b49572461ab86235297478a
Alperen YILDIRIM <alperen.yildirim36@protonmail.com>
2021-08-24 01:40:34 +0300

    Add files via upload

---
+++ Ryzen Master/ryzenmaster.ps1
@@ -0,0 +1,9 @@
+﻿$installloc = "C:\Program Files\AMD\RyzenMaster\bin"
+$installxml = "$($installloc)\AMD Ryzen Master.VisualElementsManifest.xml"
+$install150logo = "$($installloc)\Assets\150x150Logo.png"
+$install70Logo = "$($installloc)\Assets\70x70Logo.png"
+wget 'https://raw.githubusercontent.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/main/Ryzen%20Master/AMD-Ryzen-Master.VisualElementsManifest.xml' -OutFile $installxml
+New-Item -Path "$($installloc)" -Name "Assets" -ItemType "directory" | Out-Null
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/Ryzen%20Master/Assets/150x150Logo.png' -OutFile $install150logo
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/Ryzen%20Master/Assets/70x70Logo.png' -OutFile $install70Logo
+(ls "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AMD Ryzen Master\Ryzen Master\Ryzen Master.lnk").lastwritetime = get-date
