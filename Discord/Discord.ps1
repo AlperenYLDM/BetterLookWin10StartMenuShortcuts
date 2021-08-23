1865f03c07d362086680595b7ef78b57245ad360
Alperen YILDIRIM <alperen.yildirim36@protonmail.com>
2021-08-23 22:02:04 +0300

    Add files via upload

---
+++ Discord/Discord.ps1
@@ -0,0 +1,9 @@
+﻿$installloc = "C:\ProgramData\$env:UserName\Discord"
+$installxml = "$($installloc)\Update.VisualElementsManifest.xml" -replace " "*25
+$install150logo = "$($installloc)\Assets\150x150Logo.png" -replace " "*25
+$install70Logo = "$($installloc)\Assets\70x70Logo.png" -replace " "*25
+wget 'https://raw.githubusercontent.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/main/Discord/Update.VisualElementsManifest.xml' -OutFile $installxml
+New-Item -Path "$($installloc)" -Name "Assets" -ItemType "directory" | Out-Null
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/Discord/Assets/150x150Logo.png' -OutFile $install150logo
+wget 'https://github.com/AlperenYLDM/BetterLookWin10StartMenuShortcuts/raw/main/Discord/Assets/70x70Logo.png' -OutFile $install70Logo
+(ls "C:\Users\YıldırımPc\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk").lastwritetime = get-date
